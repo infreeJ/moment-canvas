b09ea1d9223db19dac77fc0d4b3f78754da2f3d0
infreeJ <kasious97@naver.com>
2025-11-21 11:50:28 +0900

    backend-init

---
+++ moment-canvas-backend/src/main/java/com/infreej/moment_canvas/MomentCanvasApplication.java
@@ -0,0 +1,13 @@
+package com.infreej.moment_canvas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomentCanvasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MomentCanvasApplication.class, args);
+	}
+
+}
---
+++ moment-canvas-backend/src/test/java/com/infreej/moment_canvas/MomentCanvasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infreej.moment_canvas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomentCanvasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
