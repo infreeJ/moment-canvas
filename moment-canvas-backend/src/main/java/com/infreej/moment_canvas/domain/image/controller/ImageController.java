4871d7bda93884cd0419bac00637137549100cab
infreeJ <kasious97@naver.com>
2025-11-25 12:26:36 +0900

    SecurityConfig 정의

--- moment-canvas-backend/src/main/java/com/infreej/moment_canvas/domain/image/controller/ImageController.java
+++ moment-canvas-backend/src/main/java/com/infreej/moment_canvas/domain/image/controller/ImageController.java
@@ -5,12 +5,15 @@ import com.infreej.moment_canvas.domain.image.service.ImageService;
 import com.infreej.moment_canvas.global.annotation.SetSuccess;
 import com.infreej.moment_canvas.global.annotation.TimeCheck;
 import com.infreej.moment_canvas.global.code.SuccessCode;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Image", description = "이미지 관련 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1")
@@ -18,6 +21,7 @@ public class ImageController {
 
     private final ImageService imageService;
 
+    @Operation(summary = "일기 이미지 생성", description = "OpenAI API를 사용하는 일기 이미지 생성 API 입니다.")
     @TimeCheck
     @SetSuccess(SuccessCode.IMAGE_GENERATED)
     @PostMapping("/image/generate")
---
+++ moment-canvas-backend/src/main/java/com/infreej/moment_canvas/global/config/SecurityConfig.java
@@ -0,0 +1,72 @@
+package com.infreej.moment_canvas.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration //설정 클래스임을 명시
+@EnableWebSecurity // Security의 웹 보완 기능을 활성화해서 모든 요청을 가로채고, 보안 규칙 적용
+@EnableMethodSecurity(securedEnabled = true) // 메서드 호출 전 권한 검사
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+//    private final JwtFilter jwtFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        // 화이트리스트
+        String[] whiteList = {
+                "/swagger-ui/**", "/v3/api-docs/**", "/error"
+        };
+
+        http
+                .csrf(AbstractHttpConfigurer::disable) // CSRF 비활성화
+                .formLogin(AbstractHttpConfigurer::disable) // Form 로그인 비활성화
+                .httpBasic(AbstractHttpConfigurer::disable) // HTTP Basic 비활성화
+
+                // 세션 설정 (Stateless)
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+
+                // 접근 권한 설정
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(whiteList).permitAll()
+//                        .requestMatchers("/admin/**").hasRole("ADMIN")
+//                        .requestMatchers("/staff/**").hasAnyRole("ADMIN", "STAFF")
+                        .requestMatchers(HttpMethod.POST, "/v1/login", "/v1/user").permitAll()
+                        .anyRequest().permitAll()
+//                        .anyRequest().authenticated() // TODO: 추후 authenticated로 변경 필요
+
+                );
+
+//                // JWT 필터 추가
+//                // JWT 검증이 먼저 이루어 지도록 커스텀 필터를 추가하고 순서 설정
+//                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(); // 비밀번호 해싱 알고리즘 객체
+    }
+
+    // 검증 시 필요한 메서드를 지정하는 메서드
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+}
